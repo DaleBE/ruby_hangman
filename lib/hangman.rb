101d240714a17c5613cc01c8f48a5db48baeab22
Dale Belcham <dandelionwishes@protonmail.ch>
2023-09-02 07:40:28 +0200

    Added save game function. Working on cleaning up classes.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 class Computer
   attr_reader :random_word
 
@@ -32,20 +34,21 @@ end
 
 class Game
   attr_reader :computer, :player, :computers_word, :display_word
+  attr_accessor :round
 
-  def initialize(computer, player)
+  def initialize(computer, player, round = 1)
     @computer = computer
     @player = player
     @computers_word = computer.random_word.split('')
     @display_word = Array.new(@computers_word.length, '_')
+    @round = round
   end
 
-  def game_start
+  def new_game_start
     puts 'The computer has chosen a word:'
     puts ''
     puts @display_word.join(' ')
     puts ''
-    puts 'You will have 12 chances to guess the correct word.'
   end
 
   def play_round
@@ -74,24 +77,62 @@ class Game
       puts @display_word.join(' ')
     end
   end
+end
+
+class GameStart
+  attr_reader :game, :choice
+
+  def initialize
+    @game = game
+    @choice = choice
+  end
+
+  def beginning
+    puts 'Welcome to the game of Hangman!'
+    puts 'The computer will choose a word and you have'
+    puts '12 chances to guess the word.'
+    puts 'Would you like to play a saved game or start anew? ( saved/new )'
+    @choice = gets.chomp.downcase
+  end
+
+  def save_game
+    puts 'Do you wish to save the game? (save)'
+    choice = gets.chomp!.downcase
+
+    return unless choice == 'save'
+
+    saved_game = File.new('saved_game.yml', 'w')
+    saved_game.puts @game.to_yaml
+    saved_game.close
+  end
+
+  def start
+    beginning
+
+    if @choice == 'new'
+      @game = Game.new(Computer.new, Player.new)
+      @game.new_game_start
+      game_sequence
+    elsif @choice == 'saved'
+      @game = Psych.safe_load_file('saved_game.yml', permitted_classes: [Game, Computer, Player])
+      puts @game.display_word.join(' ')
+      game_sequence
+    end
+  end
 
   def game_sequence
-    round = 1
+    while @game.round <= 12
+      save_game
+      puts "Round: #{@game.round}"
+      @game.play_round
+      @game.check_word
+      break if @game.computers_word == @game.display_word
 
-    game_start
-
-    while round <= 12
-      puts "Round: #{round}"
-      play_round
-      check_word
-      break if @computers_word == @display_word
-
-      round += 1
+      @game.round += 1
     end
-    puts "The word is: #{@computers_word.join('')}"
+    puts "The word is: #{@game.computers_word.join('')}"
   end
 end
 
-game = Game.new(Computer.new, Player.new)
-
-game.game_sequence
+hangman = GameStart.new
+hangman.start
